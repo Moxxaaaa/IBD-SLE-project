b75fc7234bd6210ec813fd0aec4858f65795a3a4
Moxxaaaa <150444682+Moxxaaaa@users.noreply.github.com>
2025-08-22 17:14:24 +0800

    Update 2.PLACO_calculation_loop.R

--- 5.PLACO/2.PLACO_calculation_loop.R
+++ 5.PLACO/2.PLACO_calculation_loop.R
@@ -7,13 +7,13 @@ for (j in sumstats1_for) {
     sumstats2=k
     setwd(paste0(path_prefix,file_name,"/PLACO/",sumstats1,"-",sumstats2))
     load(paste0(sumstats1,"-",sumstats2,".RData"))
-    ###设置多核并行跑
+
     cl <- makeCluster(ncore)
     clusterExport(cl,c("j","k","sumstats1","sumstats2"))
     clusterEvalQ(cl, {load(paste0(sumstats1,"-",sumstats2,".RData"))})
     out <- parSapply(cl,1:p, function(i) placo(Z=Z.matrix.decor[i,], VarZ=VarZ))
     stopCluster(cl)
-    ####整合结果到原始文件
+
     f12<-fread(paste0(sumstats1, "-",sumstats2,".txt.gz"))
     out %>% 
       t() %>% 
@@ -24,7 +24,7 @@ for (j in sumstats1_for) {
     f12 %>%
       filter(P.f1<10^-4) %>% 
       filter(P.f2<10^-4) %>%   
-      filter(p.placo<5*10^-8)->f12_sig###输出显著的点
+      filter(p.placo<5*10^-8)->f12_sig
     fwrite(f12,
            file = paste0(sumstats1, "-",sumstats2,".txt.gz"),
            compress = "gzip",
@@ -47,4 +47,5 @@ for (j in sumstats1_for) {
     
   }
   
+
 }
